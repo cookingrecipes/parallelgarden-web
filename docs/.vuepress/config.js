414889af0174afb6ad70c1e981ba70c5b9e5a4c7
TS <NA>
2019-01-31 21:43:50 +0100

    adds web content

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,67 +1,10 @@
 module.exports = {
-  title: 'Grateful mood',
-  description: 'Hello Ralu!',
+  title: 'Parallel Garden',
+  description: 'Hydroponics in a Box!',
   themeConfig: {
-      sidebar: {
-	  '/food/': [
-	      '',
-	      {	      
-		  title: 'Salads',
-		  collapsable: false,
-		  children: [
-		      'bc-salad/',
-		  ]
-	      },
-	      {	      
-		  title: 'Soups',
-		  collapsable: false,
-		  children: [
-		      'cgo-soup/',
-		  ]
-	      },
-	      {	      
-		  title: 'Lunch',
-		  collapsable: false,
-		  children: [
-		      'vegetables/',
-		      'cabbage-knedlic/',
-		  ]
-	      },
-	      {	      
-		  title: 'Deserts',
-		  collapsable: false,
-		  children: [
-		      'frangipane/',
-		      'chocolate-brownies/',
-		      'cheese-balls/',
-		      'strudel/',
-		  ]
-	      },
-	  ],
-
-	  '/blog/': [
-	      ['','some heading'],
-	      'VAN',
-	  ],
-
-	  // fallback
-	  '/': [
-              '',        /* / */
-              // 'contact', /* /contact.html */
-              // 'about'    /* /about.html */
-	  ]
-      },
+      sidebar: 'auto',
     nav: [
-{ text: 'Home', link: '/' },
-{ text: 'Blog', link: '/blog/' },
-{ text: 'Food recipes', link: '/food/' },
-// { text: 'Fermented Oddities', link: '/guide/' },
-// { text: 'Photo', link: '/guide/' },
-// { text: 'My notebooks & workshop', link: '/guide/' },
-// { text: 'Fire show', link: '/guide/' },
-// { text: 'My book', link: '/guide/' },
-// { text: 'Canvas', link: '/guide/' },
-
+	{ text: 'Parallelni Polis', link: 'https://paralelnipolis.cz' },
     ]
   }
 }
